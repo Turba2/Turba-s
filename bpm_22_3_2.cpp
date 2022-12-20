409588e3fef4935c40f518f269d7569d27c37a0f
Turba2 <113940675+Turba2@users.noreply.github.com>
2022-12-20 10:25:19 +0300

    Add files via upload

---
+++ bpm_22_3_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <chrono>
+#include <set>
+#include <stack>
+#include <vector>
+#include <time.h>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <queue>
+#include <deque>
+#include <functional>
+#include <cctype>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <geometry.hpp>
+
+int mane() { 
+    bool second(const double& x, const double& y) {
+        if (std::abs(lcpp_x:x) <= 2 && std::abs(lspp_y:y) <= 1 && x * x + y * y >= 1) {
+            return true;
+        else {
+            return false;
+        }
+        }
+    }
+
+}
+
+    
